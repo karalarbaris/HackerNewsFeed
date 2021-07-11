f15894e0cf2e548aa44c2bf53bf98f133e03ef0e
Baris <karalarbaris@gmail.com>
2021-07-11 06:16:16 +0200

    Initial Commit

---
+++ HackerNewsFeed/HackerNewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsFeedApp.swift
+//  HackerNewsFeed
+//
+//  Created by Baris Karalar on 11.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
